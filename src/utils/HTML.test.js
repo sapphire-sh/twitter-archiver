e8f9c7afb5f316da7e2f8622d45f737524896be9
sapphire <info@sapphire.sh>
2017-12-25 22:52:18 +0900

    update test

--- src/utils/HTML.test.js
+++ src/utils/HTML.test.js
@@ -2,6 +2,6 @@ import HTML from './HTML';
 
 describe('./utils/HTML.js', () => {
 	test('HTML', () => {
-		expect(HTML()).toEqual('<!DOCTYPE html><html data-reactroot=""><head><title>twitter-archiver</title><link rel="stylesheet" href="/styles.css"/></head><body><div id="app"></div><script src="/bundle.js"></script></body></html>');
+		expect(HTML()).toEqual('<!DOCTYPE html><html data-reactroot=""><head><title>twitter-archiver</title><link rel="stylesheet" href="/styles.css"/><base target="_blank"/></head><body><div id="app"></div><script src="/bundle.js"></script></body></html>');
 	});
 });
