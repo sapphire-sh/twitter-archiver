be760d925d3877ab7bfa6c304df007f973874b94
sapphire <info@sapphire.sh>
2018-07-24 23:06:21 +0900

    commit

--- src/server/routers/webhook.ts
+++ src/server/routers/webhook.ts
@@ -10,18 +10,19 @@ router.get('/', (req, res) => {
 	console.log('get');
 	console.log(req);
 	console.log(res);
-	res.json(true);
+	res.sendStatus(200).json(true);
 });
 
 router.get('/', (req, res) => {
 	console.log('post');
 	console.log(req);
 	console.log(res);
-	res.json(true);
+	res.sendStatus(200).json(true);
 });
 
 router.get('/set', (req, res) => {
 	Twitter.setWebhook();
+	res.json(true);
 });
 
 export default router;
