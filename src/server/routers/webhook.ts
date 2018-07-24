e1cf7bd91068257a11a01bf0e41133bae6f78a57
sapphire <info@sapphire.sh>
2018-07-25 00:10:37 +0900

    commit

--- src/server/routers/webhook.ts
+++ src/server/routers/webhook.ts
@@ -9,16 +9,11 @@ import {
 const router = Express.Router();
 
 router.get('/', (req, res) => {
-	console.log(req.protocol);
 	const hmac = crypto.createHmac('sha256', __env.consumer_secret);
-	console.log(req.query);
-	console.log(req.query.crc_token);
 	hmac.update(req.query.crc_token);
-	const token = hmac.digest('hex');
-	console.log(token);
 
 	res.status(200).json({
-		'response_token': token,
+		'response_token': hmac.digest('base64'),
 	});
 });
 
