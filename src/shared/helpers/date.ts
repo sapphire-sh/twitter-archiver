3fae2a97b14425d191b9ed853e3ab3cff7494ee1
sapphire <info@sapphire.sh>
2018-08-07 11:52:35 +0900

    fix test

--- src/shared/helpers/date.ts
+++ src/shared/helpers/date.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export function dateToString(date: Date) {
-	return moment(date).format('YYYY-MM-DD hh:mm:ss');
+	return moment(date).format('YYYY-MM-DD HH:mm:ss');
 }
 
 export function dateToRelativeString(date: Date) {
